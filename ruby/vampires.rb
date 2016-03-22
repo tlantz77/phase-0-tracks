a3da6fa1107559fc1533cb4f34b7c2a89cd31f65
Tim Lantz <tlantz@gmail.com>
2016-03-22 13:31:50 -0500

    Add release 1 user info code

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+#User interview
+puts "Welcome to your first interview!  Let's get started..."
+puts "What is your name?"
+  name = gets.chomp 
+puts "How old are you?"
+  age = gets.to_i
+puts "In what year were you born?"
+  birthyear = gets.to_i 
+puts "Our cafeteria serves garlic bread.  Would you like us to order some for you? (y/n)"
+  wants_garlic = gets.chomp
+puts "Would you like to enroll in our health insurance plan? (y/n)"
+  wants_insure = gets.chomp
+
+    
