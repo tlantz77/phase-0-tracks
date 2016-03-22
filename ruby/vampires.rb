4e665de3fa82d608ccc7a168e292e419d713d428
Tim Lantz <tlantz@gmail.com>
2016-03-22 16:23:58 -0500

    Add release 2 code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,8 +7,27 @@ puts "How old are you?"
 puts "In what year were you born?"
   birthyear = gets.to_i 
 puts "Our cafeteria serves garlic bread.  Would you like us to order some for you? (y/n)"
-  wants_garlic = gets.chomp
+  want_garlic = gets.chr
 puts "Would you like to enroll in our health insurance plan? (y/n)"
-  wants_insure = gets.chomp
+  want_insure = gets.chr
 
-    
+#Vampire evalution program
+currentyear = 2016
+
+if (currentyear - birthyear == age)||(currentyear - birthyear == age + 1)
+    age_verify = true
+  else
+    age_verify = false
+end
+
+if (name == "Drake Cula") || (name == "Tu Fang")
+    puts "Definitely a vampire."
+  elsif (age_verify == true) && (want_garlic == "y") && (want_insure == "y")
+    puts "Probably not a vampire."
+  elsif (age_verify == false) && (want_garlic != "y") && (want_insure != "y") 
+    puts "Almost certainly a vampire."
+  elsif (age_verify == false) && ((want_garlic != "y") || (want_insure != "y"))
+    puts "Probably a vampire."
+  else
+    puts "Results inconclusive."
+end    
