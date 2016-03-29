05ab5e0dde03dd4e80d69dd7a2f24d0138b3bf15
Tim Lantz <tlantz@gmail.com>
2016-03-28 19:27:33 -0500

    Add method calls to method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,45 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN"
 
-# “zom”.<???>
-# => “zoom”
+"zom".insert(2, 'o')
+"zom".sub('o', 'oo')
+"zom".gsub('o', 'oo')
+# => "zoom"
 
-# “enhance”.<???>
-#=> “    enhance    ”
+"enhance".center(15)
+"enhance".ljust(11).rjust(15)
+#=> "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+"Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
 
-# “the usual”.<???>
-#=> “the usual suspects”
+"the usual" << " suspects"
+"the usual".replace("the usual suspects")
+" the usual".gsub(/$/, " suspects")
+#=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+" suspects".prepend("the usual")
+" suspects".replace("the usual suspects")
+#=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".slice(0..-2)
+# => "The case of the disappearing last lette"
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+"The mystery of the missing first letter".slice(1..-1)
+"The mystery of the missing first letter".tr('T', '')
+# => "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+"Elementary,    my   dear      Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
 
-#“z”.<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the decimal ASCII code for a lowercase 'z'.
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+"How many times does the letter ‘a’ appear in this string?".count('a')
 #=> 4
