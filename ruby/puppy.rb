50289939fc0332688b5430321fce51ca17778dcd
Tim Lantz <tlantz@gmail.com>
2016-04-04 17:45:55 -0500

    Add puppy.class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+#Puppy class
+#SPECIES --------------------
+#Poopus Maximus
+#
+#CHARACTERISTICS ------------
+#Paws: 4
+#Ears: 2
+#Color: varies
+#Good guard dog: yes
+#Good w/ children: yes
+#Favorite Game: Fetch ball
+#Mortal Enemy: squeaky toys
+#
+#BEHAVIOR -------------------
+#Poop
+#Pee
+#Run
+#Sniff
+#Eat
+#Bark
+
