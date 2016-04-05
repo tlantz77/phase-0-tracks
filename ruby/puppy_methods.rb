2d45f77f00f90fbf78d218119c7d0645a368d55a
Tim Lantz <tlantz@gmail.com>
2016-04-05 15:25:55 -0500

    Add release 2 content

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,6 +1,7 @@
 class Puppy
   def initialize(name)
-    @name = name 
+    @name = name
+  
   end
 
   def fetch(toy)
@@ -31,8 +32,45 @@ class Puppy
 end
 
 
-sadie = Puppy.new("Sadie")
-p sadie.fetch("bone")
+#sadie = Puppy.new("Sadie")
+#p sadie.fetch("bone")
+
+class Car
+  def initialize(name)
+    @name = name
+  end
+  
+  def drive(speed)
+     "zoom zoom at #{speed.to_i} MPH" 
+  end
+  
+  def pop_a_wheely
+     "Popping those wheels"
+  end
+  
+  def breakdown(money)
+    "The breakdown will cost you $ #{money.to_i}"
+  end
+  
+  def name
+    @name
+  end
+
+end
+
+car_array = []
+50.times do |x|
+  #New Class 
+  car = Car.new("Car number #{x.to_s}") 
+  car_array << car
+end
 
 
 
+car_array.each do |car|
+  p car.name 
+  p car.drive(Random.rand(55))
+  p car.pop_a_wheely
+  p car.breakdown(Random.rand(550))
+
+end
