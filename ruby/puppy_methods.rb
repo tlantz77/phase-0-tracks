0ea8e493becfeaa40c94628df66a9c32d1142d1c
Tim Lantz <tlantz@gmail.com>
2016-04-05 14:18:42 -0500

    Add release 0 content

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,27 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(int)
+    count = 0
+    while count <= int
+      puts "Woof!"
+      count += 1
+    end  
+  end
+  
+  def roll_over
+    puts "Roll over!"
+  end
+  
+  def dog_years(age)
+    d_age = age / 7
+  end
+  
+  def shake
+    put "Shake!!!!"
+  end
+
 
 end
 
