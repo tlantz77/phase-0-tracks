ccebb4da3900824c7df13eea8334d22eb95a45df
Tim Lantz <tlantz@gmail.com>
2016-04-06 18:56:44 -0500

    Add release 0 content (pseudocode)

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+# Class - Viking Warrior
+# Attributes
+# -----------------------
+# Age (Integer)
+# Favorite Weapon (String)
+# Favorite God (String)
+# Wearing Helmet? (True/False)
+
+# Methods
+# -----------------------
+# Don Helmet (can change Wearing Helmet?)
+# Pray to God (can change Patron God)
+# Go Berserk!
+
