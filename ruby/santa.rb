29472eaf2d1d561a6b3b9fe569ca590f1460b244
Tim Lantz <tlantz@gmail.com>
2016-04-06 15:56:11 -0500

    Add release 2 content

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,8 +4,8 @@ class Santa
     p "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
   
   def speak
@@ -15,10 +15,28 @@ class Santa
   def eat_milk_and_cookies(cookie)
     p "That was a good #{cookie}!"
   end
+  
+  #getter methods
+  def gender
+    @gender
+  end
+  
+  def ethnicity
+    @ethnicity
+  end
+  
+  #setter methods
+  def celebrate_birthday
+    @age += 1
+  end  
+  
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.push(reindeer)
+  end
 end
 
-
-#Random santa generator
+#random Santas generator
 def random_santas(x)
   santas = []
   genders = ["male", "female", "genderfluid", "transgender", "agender", "indeterminate"]
@@ -34,4 +52,14 @@ puts "How many Santas would you like to generate?"
 input = gets.to_i
 santas = random_santas(input)
 p santas
+  
+santas[0].celebrate_birthday
+
+p santas
+
+santas[2].get_mad_at("Rudolph")
+
+p santas
+
+
 
