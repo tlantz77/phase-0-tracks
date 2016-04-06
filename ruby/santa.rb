dd325a17fcc9413131381d872915469547f99745
Tim Lantz <tlantz@gmail.com>
2016-04-06 14:24:34 -0500

    Add release 1 content

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,19 +1,37 @@
 #define Santa class
 class Santa
-	def initialize
-		p "Initializing Santa instance..."
-	end
-	
-	def speak
-		p "Ho, ho, ho! Haaaappy holidays!"
-	end	
-	
-	def eat_milk_and_cookies(cookie)
-		p "That was a good #{cookie}!"
-	end
+  def initialize(gender, ethnicity)
+    p "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+  
+  def speak
+    p "Ho, ho, ho! Haaaappy holidays!"
+  end  
+  
+  def eat_milk_and_cookies(cookie)
+    p "That was a good #{cookie}!"
+  end
 end
 
-george = Santa.new
-george.speak
-george.eat_milk_and_cookies("peanut butter")
+
+#Random santa generator
+def random_santas(x)
+  santas = []
+  genders = ["male", "female", "genderfluid", "transgender", "agender", "indeterminate"]
+  ethnicities = ["human", "elf", "dwarf", "hobbit", "orc", "troll", "dragon"]
+   x.times do 
+    santas << Santa.new(genders[Random.rand(genders.length)], ethnicities[Random.rand(ethnicities.length)])
+  end  
+  santas
+end
+
+#driver code
+puts "How many Santas would you like to generate?"
+input = gets.to_i
+santas = random_santas(input)
+p santas
 
