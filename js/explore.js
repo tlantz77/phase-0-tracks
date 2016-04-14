3ad0adb1bd3b07b8827d1d9687ee22e1067fb161
Tim Lantz <tlantz@gmail.com>
2016-04-14 15:12:08 -0500

    Add reverse string function and driver code

--- js/explore.js
+++ js/explore.js
@@ -3,5 +3,20 @@
 - Loop through the string
 	- Take the last letter first, add it to the empty string.
 	- Proceed to each previous letter in the string, adding it to the new string, until there are no letters left.
-	- Print the new string. 
+	- Return and Print the new string. 
 */
+
+function reverse(str) {
+	var ReverseStr = "";
+	for (var i = (str.length-1); i >= 0; i--) {
+		ReverseStr = ReverseStr + str[i];
+	}
+	console.log(ReverseStr);
+}
+
+reverse("Hello World!");
+
+var bear = "Winnie the Pooh";
+
+console.log(bear);
+reverse(bear);
