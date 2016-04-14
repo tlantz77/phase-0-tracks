dcd936c6b9bcb4d16fb5c573acc18d393333e28a
Tim Lantz <tlantz@gmail.com>
2016-04-14 15:01:32 -0500

    Add reverse string pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+/*Pseudocode - reverse letters
+- Define a variable as an empty string to house our reversed word.
+- Loop through the string
+	- Take the last letter first, add it to the empty string.
+	- Proceed to each previous letter in the string, adding it to the new string, until there are no letters left.
+	- Print the new string. 
+*/
