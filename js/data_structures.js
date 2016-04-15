8b7c7a1ba95f20696da25790631968ee8074d36f
Tim Lantz <tlantz@gmail.com>
2016-04-15 10:44:57 -0500

    Add horse and color arrays

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+//Horse and color arrays
+
+var colors = ["blue", "red", "yellow", "green"];
+
+var names = ["George", "Bob", "Lucy", "Charlie"];
+
+colors.push("purple");
+
+names.push("Ed");
+
+console.log(colors, names);
