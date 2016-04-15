c9069918e27ef964258b0bdbd8ba2ae2490b0d0c
Tim Lantz <tlantz@gmail.com>
2016-04-15 11:16:16 -0500

    Add car function and drivder code

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,3 +19,25 @@ for (var i = 0; i < names.length; i++) {
 }
 console.log(horses);
 
+
+
+function Car(color, year, isManual) {
+  this.color = color;
+  this.year = year;
+  this.isManual = isManual;
+  
+  this.drive = function() {
+    console.log("Vroom!");
+  };
+}
+
+// create a few different cars
+
+var newCar = new Car("blue", 2010, false);
+var oldCar = new Car("green", 1982, true);
+var fastCar = new Car("red", 2017, true);
+
+newCar.drive();
+oldCar.drive();
+
+console.log(newCar, oldCar, fastCar);
