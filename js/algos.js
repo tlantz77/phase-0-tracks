eaf68a58267ae590ddf2f9331c32150ae4fa5014
Tim Lantz <tlantz@gmail.com>
2016-04-16 13:45:49 -0500

    Add release 1 content - object matching function

--- js/algos.js
+++ js/algos.js
@@ -28,3 +28,38 @@ longestStr(array);
 var bugs = ["ant", "beetle", "tarantulas", "bee", "wasp", "bumblebee"];
 
 longestStr(bugs);
+
+/*Release 1 Pseudocode
+- Declare a boolean variable to check if a match is found and set it to false.
+- Iterate one-by-one through the keys of the first object
+	- For each key in that object, iterate through the keys of the second object.
+		- if a key match is found, check the values of both keys.
+			- if the values are equal, set boolean variable to true.
+		- otherwise do nothing.	
+- Return value of the boolean.*/
+
+var thingA = {color: "blue", num: 3, isCool: false};
+var thingB = {color: "red", num: 4, isNeat: false};
+var thingC = {color: "yellow", num: 3, isAwesome: true};
+
+function compareObj(obj1, obj2) {
+	var match = false;
+	for (var key1 in obj1) {
+		for (var key2 in obj2) {
+			if (key1 == key2) {
+				if (obj1[key1] == obj2[key2]) {
+					match = true;
+				}
+			}
+		}
+	}
+	return match;
+}
+
+console.log(compareObj(thingA, thingB));
+
+//"num: 3" is the only matching key-value pair so this is the only that should return true.
+console.log(compareObj(thingA, thingC));
+
+console.log(compareObj(thingB, thingC));
+
