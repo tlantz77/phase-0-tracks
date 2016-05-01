585c95b03d20c6f312e126c8ffacc3fc17cf1064
Tim Lantz <tlantz@gmail.com>
2016-05-01 17:24:02 -0500

    Make minor edits

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
 	$("#name").hide();
 	$("#hamster").hover(function() {
-		$("img").hide();
-		$("#name").show();
-	},
-	function() {
-		$("img").show();
-		$("#name").hide();
-	}
-	);
+		$("img").toggle(100);
+		$("#name").toggle(100);
+	});	
 });
 
 
