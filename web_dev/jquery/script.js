588b95996994c0ec7ed00b2c4666a7503bf0291d
Tim Lantz <tlantz@gmail.com>
2016-05-01 18:07:07 -0500

    Add image and jquery function for second hamster

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 	$("#name").hide();
-	$("#hamster").hover(function() {
-		$("img").toggle(100);
+	
+	$("#hamster1").hover(function(){
+		$("#h1").toggle(100);
 		$("#name").toggle(100);
 	});	
+
+	$("#hamster2").click(function(){
+		$("#panel").slideToggle("slow");
+	});
 });
 
 
